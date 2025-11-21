efe1deed18b8a50f80b5ae24b093379920ad2c65
abhinayh56 <kumarabhinay53@gmail.com>
2025-11-21 18:22:10 +0530

    asdfsfd

--- Class1.h
+++ Class1.h
@@ -8,7 +8,9 @@
 class Class1
 {
 public:
-    Class1()
+    Class1() : de_1("KEY_11", "PATH_11", 0),
+               de_2("KEY_12", "PATH_12", 0),
+               de_3("KEY_13", "PATH_13", 0)
     {
     }
 
@@ -20,7 +22,7 @@ public:
     {
     }
 
-    Data_element<uint32_t> de_1{"KEY_11", "PATH_11", 0};
-    Data_element<uint64_t> de_2{"KEY_12", "PATH_12", 0};
-    Data_element<int8_t> de_3{"KEY_13", "PATH_13", 0};
+    Data_element<uint32_t> de_1;
+    Data_element<uint64_t> de_2;
+    Data_element<int8_t> de_3;
 };
--- Class2.h
+++ Class2.h
@@ -8,7 +8,9 @@
 class Class2
 {
 public:
-    Class2()
+    Class2() : de_1("KEY_21", "PATH_21", "0"),
+               de_2("KEY_22", "PATH_22", 0),
+               de_3("KEY_23", "PATH_23", 0)
     {
     }
 
@@ -20,7 +22,7 @@ public:
     {
     }
 
-    Data_element<std::string> de_1{"KEY_21", "PATH_21", "0"};
-    Data_element<uint8_t> de_2{"KEY_22", "PATH_22", 0};
-    Data_element<long long> de_3{"KEY_23", "PATH_23", 0};
+    Data_element<std::string> de_1;
+    Data_element<uint8_t> de_2;
+    Data_element<long long> de_3;
 };
