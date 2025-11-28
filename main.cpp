3f07586aa222005e624383a27024bdedce6e4ddb
abhinayh56 <kumarabhinay53@gmail.com>
2025-11-28 18:29:36 +0530

    sdfasf

--- main.cpp
+++ main.cpp
@@ -1,16 +1,28 @@
 #include <stdint.h>
 #include <iostream>
-#include "Class1.h"
-#include "Class2.h"
+#include <fstream>
 #include "Class_sensor.h"
+#include <map>
 
 int main()
 {
-    Class_sensor class_sensor;
+    std::ifstream file("config.txt");
+    if (!file.is_open())
+    {
+        std::cerr << "Failed to open config.txt" << std::endl;
+        return 1;
+    }
 
-    class_sensor.fun_sensor();
-    
-    class_sensor.disp_data_elements();
+    std::string line;
+    while (std::getline(file, line))
+    {
+        std::cout << line << std::endl;
+    }
+    file.close();
+
+    // Class_sensor class_sensor;
+    // class_sensor.fun_sensor();
+    // class_sensor.disp_data_elements();
 
     return 0;
 }
